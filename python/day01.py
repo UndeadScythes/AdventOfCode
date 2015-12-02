2025cb8d536eb5fd045d5d284922e53e363a4e91
UndeadScythes <daveyognaut2@gmail.com>
2015-12-02 20:55:07 +0000

    Day 01

---
+++ python/day01.py
@@ -0,0 +1,14 @@
+instructions = open("../input/day01.txt", "r").read()
+
+floor    = 1
+basement = -1
+
+for i in range(len(instructions)):
+    
+    floor += 1 if instructions[i] == "(" else -1
+
+    if floor == -1 and basement == -1:
+        
+        basement = i + 1
+        
+print("Floor: {0}, Basement: {1}".format(floor, basement))
